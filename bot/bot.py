a6b81ad0f43c9ba753d8ae9a6aba9bdeebc45b84
wolendranh <hulpa.yura@gmail.com>
2018-12-27 10:25:06 +0200

    WIP

--- bot/bot.py
+++ bot/bot.py
@@ -12,13 +12,13 @@ def echo(bot, update):
 if __name__ == '__main__':
     TOKEN = os.environ.get('TOKEN', '')
     PORT = int(os.environ.get('PORT', '8443'))
-    updater = Updater('736783073:AAHp2vnt5yZGU1kLh1iuB9y1ZgiuuW07_G4')
+    updater = Updater(TOKEN)
     echo_handler = MessageHandler(Filters.text, echo)
     updater.dispatcher.add_handler(echo_handler, group=1)
 
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
-                          url_path='736783073:AAHp2vnt5yZGU1kLh1iuB9y1ZgiuuW07_G4')
-    updater.bot.set_webhook("https://66fa704b.ngrok.io/" + '736783073:AAHp2vnt5yZGU1kLh1iuB9y1ZgiuuW07_G4')
+                          url_path=TOKEN)
+    updater.bot.set_webhook("https://66fa704b.ngrok.io/" + TOKEN)
     updater.idle()
 
