d82281f0a9dec1902cf2a662a320cfcfb2cb4d31
wolendranh <hulpa.yura@gmail.com>
2019-01-07 17:12:28 +0200

    ignore idea

--- flying_bear_bot/project_settings.py
+++ flying_bear_bot/project_settings.py
@@ -19,7 +19,9 @@ if HEROKU:
 
     import dj_database_url
     # Parse database configuration from $DATABASE_URL
-    DATABASES['default'] =  dj_database_url.config()
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
