27a52176e960b8d7cae964893a0df68460a92df8
wolendranh <hulpa.yura@gmail.com>
2019-01-07 17:07:35 +0200

    ignore idea

--- flying_bear_bot/project_settings.py
+++ flying_bear_bot/project_settings.py
@@ -7,26 +7,10 @@ env = environ.Env(
     DEBUG=(bool, False),
     HEROKU=(bool, False),
     BOT_WEBHOOK=(str, '')
-)  # set default values and casting
-environ.Env.read_env()  # reading .env_example file
+) # set default values and casting
+environ.Env.read_env() # reading .env_example file
 
 HEROKU = env('HEROKU')
-DATABASES = {
-    'default': {
-        'ENGINE': env('DB_ENGINE'),
-        #'USER': env('DB_USER'),
-        'NAME': env('DB_NAME'),
-        #'PASSWORD': env('DB_PASS'),
-        'TEST': {
-            'NAME': 'test_flying_bear_bot',
-        },
-    },
-}
-TELEGRAM_BOT = [{
-    'token': env('TOKEN'),
-    'register': env('BOT_REGISTER_METHOD'), 
-    'webhook': env('BOT_WEBHOOK')  
-}]
 
 ALLOWED_HOSTS = ['*']
 
@@ -35,7 +19,7 @@ if HEROKU:
 
     import dj_database_url
     # Parse database configuration from $DATABASE_URL
-    DATABASES['default'] = dj_database_url.config()
+    DATABASES['default'] =  dj_database_url.config()
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -47,3 +31,20 @@ if HEROKU:
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
 )
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': env('DB_ENGINE'),
+            #'USER': env('DB_USER'),
+            'NAME': env('DB_NAME'),
+            #'PASSWORD': env('DB_PASS'),
+            'TEST': {
+                'NAME': 'test_flying_bear_bot',
+            },
+        },
+    }
+    TELEGRAM_BOT = [{
+        'token': env('TOKEN'),
+        'register': env('BOT_REGISTER_METHOD'),
+        'webhook': env('BOT_WEBHOOK')
+    }]
